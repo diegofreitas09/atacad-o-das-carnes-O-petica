4f48fb0ee52f2d6ddb41b00579e32cad409ef99a
diegofreitas09 <diegofreitas090707@gmail.com>
2026-08-23 10:25:53 -0300

    Adiciona Instagram oficial do O Peitica

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ window.APP_CONFIG = {
   WHATSAPP: '5585989626829',
   ADDRESS: 'Rua Edison Martins, 530, Fortaleza - CE',
   MAPS_URL: 'https://www.google.com/maps/search/?api=1&query=Rua%20Edison%20Martins%2C%20530%2C%20Fortaleza%20-%20CE',
-  INSTAGRAM_URL: '',
+  INSTAGRAM_URL: 'https://www.instagram.com/atacadaodacarneopeitica?igsi=cDUyODF0ZTl5OGdv',
   // Cole aqui a URL CSV publicada do Google Sheets.
   // Ex.: https://docs.google.com/spreadsheets/d/e/SEU_ID/pub?gid=0&single=true&output=csv
   SHEET_CSV_URL: '',
