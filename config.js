3b9ab44c8d46d9305980e739a26a93d6e15b095d
diegofreitas09 <diegofreitas090707@gmail.com>
2026-08-23 11:26:08 -0300

    Prepara integração de orçamentos com Google Sheets

--- config.js
+++ config.js
@@ -5,8 +5,11 @@ window.APP_CONFIG = {
   ADDRESS: 'Rua Edison Martins, 530, Fortaleza - CE',
   MAPS_URL: 'https://www.google.com/maps/search/?api=1&query=Rua%20Edison%20Martins%2C%20530%2C%20Fortaleza%20-%20CE',
   INSTAGRAM_URL: 'https://www.instagram.com/atacadaodacarneopeitica?igsi=cDUyODF0ZTl5OGdv',
-  // Cole aqui a URL CSV publicada do Google Sheets.
+  // URL CSV pública dos produtos/preços.
   SHEET_CSV_URL: '',
+  // URL do Web App do Google Apps Script que receberá os orçamentos.
+  // Depois de publicar o arquivo google-apps-script.gs, cole aqui a URL /exec.
+  ORDER_WEBAPP_URL: '',
   DELIVERY_FEE: 0,
   DATA_TIMEOUT_MS: 7000
 };
