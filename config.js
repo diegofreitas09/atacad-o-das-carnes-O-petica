2c168e4b688c43dcc7d3acd4cf9a7b1c25aef0b1
diegofreitas09 <diegofreitas090707@gmail.com>
2026-08-23 10:46:16 -0300

    Usa link oficial do WhatsApp Business

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 window.APP_CONFIG = {
   BUSINESS_NAME: 'Atacadão da Carne O Peitica',
   WHATSAPP: '5585989626829',
+  WHATSAPP_BUSINESS_URL: 'https://wa.me/message/NW4MC6V5OVEIK1',
   ADDRESS: 'Rua Edison Martins, 530, Fortaleza - CE',
   MAPS_URL: 'https://www.google.com/maps/search/?api=1&query=Rua%20Edison%20Martins%2C%20530%2C%20Fortaleza%20-%20CE',
   INSTAGRAM_URL: 'https://www.instagram.com/atacadaodacarneopeitica?igsi=cDUyODF0ZTl5OGdv',
   // Cole aqui a URL CSV publicada do Google Sheets.
-  // Ex.: https://docs.google.com/spreadsheets/d/e/SEU_ID/pub?gid=0&single=true&output=csv
   SHEET_CSV_URL: '',
   DELIVERY_FEE: 0,
   DATA_TIMEOUT_MS: 7000
